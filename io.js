a87666ab8232d2077e26fda3850416740a8420e7
Michael Duran <mad2116@gmail.com>
2016-03-17 05:09:32 -0700

    add socket.io client emit msg w/Q data and server listen new Q data

--- io.js
+++ io.js
@@ -13,8 +13,8 @@ io.on('connection', function(socket) {
   console.log('Client connected to socket.io!');
 
   // server listen for user submit new question
-  socket.on('io-q-submitted', function(data){
-
+  socket.on('io-q-submit', function(qData){
+    console.log('GOT MESSAGE FROM CLIENT WITH DATA: ', qData);
   });
 
 
--- public/js/client_socketio.js
+++ public/js/client_socketio.js
@@ -1,10 +1,25 @@
 $(function() {
-  var ioQSubmit = '';
+
+  var ioQSubmit = '#io-q-submit-btn';
   var $ioQSubmit = $(ioQSubmit);
 
   var socket = io()
   console.log('Logging socket.io from client: ', socket);
 
-
+  $ioQSubmit.on('click', function(evt) {
+    // evt.preventDefault();
+    var qData = getQFormData();
+    console.log(qData);
+    socket.emit('io-q-submit', qData);
+  });
 
 });
+
+function getQFormData() {
+  return {
+      qAuthorId: userId,
+      qTitle:    $('#q-form-title').val(),
+      qBody:     $('#q-form-body').val(),
+      qTags:     $('#q-form-tags').val()
+    };
+}
